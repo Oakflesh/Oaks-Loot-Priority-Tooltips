f1ba42195de6084d79457d3f9379c709b5c96975
Oakflesh <Oakflesh@gmail.com>
2020-12-01 03:07:54 +0000

    First Version :D

---
+++ main.lua
@@ -0,0 +1,60 @@
+-- Add to in-game tooltips at mouse over (Like atlas loot)
+GameTooltip:HookScript("OnTooltipSetItem", function(tooltip, ...)
+    addToolTip(tooltip)
+end)
+
+-- Add to in-game tooltips on click (Like chat links)
+ItemRefTooltip:HookScript("OnTooltipSetItem", function(tooltip, ...)
+    addToolTip(tooltip)
+end)
+
+function addToolTip(tooltip)
+    local itemname, itemlink = tooltip:GetItem()
+    
+    if itemlink then
+      priority = searchForItemPrio(itemlink:match("item:(%d+):"))
+    end
+  
+    if priority then
+        priority = string.format("%s", priority)
+        priority = classColour(priority)
+        tooltip:AddLine("Prio: "..priority, 1, 1, 1)
+
+        note = searchForItemNote(itemlink:match("item:(%d+):"))
+        if note ~= "" then
+            note = string.format("%s", note)
+            tooltip:AddLine("Note: "..note, 1, 1, 1)
+        end
+    end
+end
+
+-- Add class colours to each string returned from the loot tables
+function classColour(priority) 
+    priority = priority:gsub("Druid", "|cFFFF7C0ADruid|r")
+    priority = priority:gsub("Hunter", "|cFFAAD372Hunter|r")
+    priority = priority:gsub("Mage", "|cFF3FC7EBMage|r")
+    priority = priority:gsub("Priest", "|cFFFFFFFFPriest|r")
+    priority = priority:gsub("Rogue", "|cFFFFF468Rogue|r")
+    priority = priority:gsub("Shaman", "|cFF0070DDShaman|r")
+    priority = priority:gsub("Warlock", "|cFF8788EEWarlock|r")
+    priority = priority:gsub("Warrior", "|cFFC69B6DWarrior|r")
+    return priority
+end
+
+-- look for a item priority from the relevant loot table
+function searchForItemPrio(itemname)
+    for index, value in next, SPS_Naxx_Loot do
+        if value["itemid"] == itemname then
+            return value["classPriority"]
+        end
+    end
+end
+
+-- look for a item note from the relevant loot table
+function searchForItemNote(itemname)
+    for index, value in next, SPS_Naxx_Loot do
+        if value["itemid"] == itemname then
+            return value["note"]
+        end
+    end
+end
---
+++ naxxramas.lua
@@ -0,0 +1,27 @@
+SPS_Naxx_Loot = {
+-- Abomination Wing --
+	-- Patchwerk
+    {["itemid"] = "22960", ["itemname"] = "Cloak of Suturing", ["classPriority"] = "Healers", ["note"] = ""},
+    {["itemid"] = "22961", ["itemname"] = "Band of Reanimation", ["classPriority"] = "Hunter", ["note"] = ""},
+    {["itemid"] = "22815", ["itemname"] = "Severance", ["classPriority"] = "Shaman (Enhance)", ["note"] = ""},
+    {["itemid"] = "22820", ["itemname"] = "Wand of Fates", ["classPriority"] = "Warlock > Mage > Priest (Shadow)", ["note"] = ""},
+    {["itemid"] = "22818", ["itemname"] = "The Plague Bearer", ["classPriority"] = "Warrior", ["note"] = ""},
+    -- Grobbulus
+    {["itemid"] = "22967", ["itemname"] = "Icy Scale Spaulders", ["classPriority"] = "Hunter = Shaman (Enhance) > Warrior", ["note"] = ""},
+    {["itemid"] = "22810", ["itemname"] = "Toxin Injector", ["classPriority"] = "None", ["note"] = ""},
+    {["itemid"] = "22988", ["itemname"] = "The End of Dreams", ["classPriority"] = "Priest (Shadow) = Druid (Resto) > Druid (Cat)", ["note"] = "*AQ Mace is 5 less AP, but 70+ Armour"},
+    {["itemid"] = "22803", ["itemname"] = "Midnight Haze", ["classPriority"] = "Priest (Shadow) > Casters", ["note"] = ""},
+    {["itemid"] = "22968", ["itemname"] = "Glacial Mantle", ["classPriority"] = "None", ["note"] = ""},
+    -- Gluth
+    {["itemid"] = "22994", ["itemname"] = "Digested Hand of Power", ["classPriority"] = "None", ["note"] = ""},
+    {["itemid"] = "22983", ["itemname"] = "Rime Covered Mantle", ["classPriority"] = "Mage > Priest (Shadow) = Warlock", ["note"] = ""},
+    {["itemid"] = "22813", ["itemname"] = "Claymore of Unholy Might", ["classPriority"] = "None", ["note"] = ""},
+    {["itemid"] = "22981", ["itemname"] = "Gluth's Missing Collar", ["classPriority"] = "Warrior", ["note"] = ""},
+    {["itemid"] = "23075", ["itemname"] = "Death's Bargain", ["classPriority"] = "None", ["note"] = ""},
+    -- Thaddius
+    {["itemid"] = "23070", ["itemname"] = "Leggings of Polarity", ["classPriority"] = "Mage = Warlock", ["note"] = ""},
+    {["itemid"] = "22801", ["itemname"] = "Spire of Twilight", ["classPriority"] = "Priest (Holy) = Druid (Resto)", ["note"] = ""},
+    {["itemid"] = "23001", ["itemname"] = "Eye of Diminution", ["classPriority"] = "None", ["note"] = ""},
+    {["itemid"] = "23000", ["itemname"] = "Plated Abomination Ribcage", ["classPriority"] = "Warrior (DPS)", ["note"] = ""},
+    {["itemid"] = "22808", ["itemname"] = "The Castigator", ["classPriority"] = "Rogue* > Warrior (DPS) = Rogue", ["note"] = "*Rogue has Anubisath Warhammer"},
+}
