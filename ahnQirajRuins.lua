cd83d8dd83e286ea329df6ee8cbcaa424adfe2d5
Oakflesh <Oakflesh@gmail.com>
2020-12-02 11:29:49 +0000

    Added other Raids + Expanded search function, cleared up some variables

---
+++ ahnQirajRuins.lua
@@ -0,0 +1,4 @@
+-- Follow the Naxxramas lua for an example
+ahnQirajRuins = {
+    {["itemId"] = "", ["itemName"] = "", ["classPriority"] = "", ["note"] = ""},
+}
---
+++ moltenCore.lua
@@ -0,0 +1,4 @@
+-- Follow the Naxxramas lua for an example
+moltenCore = {
+    {["itemId"] = "", ["itemName"] = "", ["classPriority"] = "", ["note"] = ""},
+}
